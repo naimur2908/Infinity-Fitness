f9453ee814c2247d6d8009d7cda8d5eb38e63ae0
generic <naimurrahman2908291@gmail.com>
2025-05-29 17:51:53 +0200

    Bokachoda

--- main.cpp
+++ main.cpp
@@ -13,6 +13,6 @@ int main() {
 
     return 0;
 
-    //bhallagena
+    //bhallagenaa
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
